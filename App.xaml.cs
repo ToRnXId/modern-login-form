5360d7e3089c1e1049d23807e98f09fa2915c6a2
Mykhailo Guivan <tornxid123@gmail.com>
2024-04-13 18:26:07 +0200

    starting project

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace modern_login_form
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
