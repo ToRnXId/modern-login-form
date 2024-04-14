0b2118ce7f66572c63c5a1af8a73a688428b1241
Mykhailo Guivan <tornxid123@gmail.com>
2024-04-14 21:23:54 +0200

    sqlconnection

---
+++ Models/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modern_login_form.Models
+{
+    public interface IUserRepository
+    {
+        bool AuthentificateUser(NetworkCredential credential);
+        void Add(UserModel userModel);
+        void Edit(UserModel userModel);
+        void Remove(int id);
+        UserModel GetById(int id);
+        UserModel GetByUserName(string userName);
+        IEnumerable<UserModel> GetAll();
+        // ...
+    }
+}
